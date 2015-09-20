052c1a745c90cdc4a2bb7be21ebe279625f09048
Vesihiisi <alicia@fagerving.se>
2015-09-20 13:24:41 +0200

    connect autocomplete to database

--- autocomplete.php
+++ autocomplete.php
@@ -1,6 +1,39 @@
 <?php
 
+define('DATABASE', __DIR__ . "/db/turf.sqlite", true);
+
+function dump($array)
+    {
+    echo "<pre>" . htmlentities(print_r($array, 1)) . "</pre>";
+    }
+
+function connectToDb($filename)
+    {
+    $dsn = "sqlite:$filename";
+    try
+        {
+        $db = new PDO($dsn);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+        }
+
+    catch(PDOException $e)
+        {
+        echo "Failed to connect to the database using DSN:<br />$dsn<br />";
+        throw $e;
+        }
+    }
+
 $term = trim(strip_tags($_GET['term']));
+$term = "%".$term."%";
+
+$db = connectToDb(DATABASE);
+
+$sql = "SELECT name FROM zones WHERE name LIKE ?";
+$params = [$term];
+$stmt = $db->prepare($sql);
+$stmt->execute($params);
+$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $allZones = array(
     "Backaplaneten",
@@ -23,10 +56,8 @@ $allZones = array(
 
 $suggestions = array();
 
-for ($i = 0; $i < count($allZones); ++$i) {
-    if (stripos($allZones[$i], $term) !== false) {
-        array_push($suggestions, $allZones[$i]);
-    }
+for ($i = 0; $i < count($res); ++$i) {
+        array_push($suggestions, $res[$i]["name"]);
 }
 
 echo json_encode($suggestions);
