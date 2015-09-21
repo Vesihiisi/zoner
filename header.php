1075ccb668072acd1b0d0d24005b2e511edb71d0
Vesihiisi <alicia@fagerving.se>
2015-09-21 12:39:03 +0200

    add navbar

--- header.php
+++ header.php
@@ -4,5 +4,9 @@
     <input type="search" name="z" id="z" placeholder="Enter search term." value="" autofocus>
     <input type="submit" value="Search">
 </form>
+<nav class="nav-main">
+<a href="">ZoneInfo</a>
+<a href="">UserInfo</a>
+</nav>
 </div>
 </header>
