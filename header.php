b30b5f0ba7ae01d977ebefea6233daa89c70a6ac
Vesihiisi <alicia@fagerving.se>
2015-09-21 12:28:41 +0200

    break out header

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+<div class="header-content">
+    <form>
+    <input type="search" name="z" id="z" placeholder="Enter search term." value="" autofocus>
+    <input type="submit" value="Search">
+</form>
+</div>
+</header>
--- zoner.php
+++ zoner.php
@@ -134,14 +134,7 @@ EOD;
 </head>
 <body>
 
-<header>
-<div class="header-content">
-    <form>
-    <input type="search" name="z" id="z" placeholder="Enter search term." value="" autofocus>
-    <input type="submit" value="Search">
-</form>
-</div>
-</header>
+<?php include("header.php");?>
 
 <main>
 <div class="map" id ="map">
