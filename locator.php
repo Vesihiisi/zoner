44c4381d1686a72db32f8974ade0cdaeecbab1d8
Vesihiisi <alicia@fagerving.se>
2015-09-21 11:27:41 +0200

    use existing function

--- locator.php
+++ locator.php
@@ -2,23 +2,13 @@
 
 include 'utils.php';
 
-// $north = 57.7114380143323; // lat
-// $south =  57.70226698381177; // lat
-// $east = 11.947417259216307; // long
-// $west = 11.928491592407227; // long
-// 
 $north = $_POST["north"]; // lat
 $south =  $_POST["south"]; // lat
 $east = $_POST["east"]; // long
 $west = $_POST["west"]; // long
 $name = $_POST["name"];
 
-$db = connectToDb(DATABASE);
+
 $sql = "SELECT * FROM zones WHERE latitude > ? AND latitude < ? AND longitude > ? AND longitude < ? AND NOT (name = ?) LIMIT 50";
 $params = [$south, $north, $west, $east, $name];
-$stmt = $db->prepare($sql);
-$stmt->execute($params);
-$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$resJson = json_encode($res);
-
-echo $resJson;
+echo json_encode(getFromDb($sql, $params));
