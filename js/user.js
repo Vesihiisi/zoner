931aa578f1568c9a210ed48b3bea6a2938bd3e06
Vesihiisi <alicia@fagerving.se>
2015-09-22 11:50:48 +0200

    implement color coding

--- js/user.js
+++ js/user.js
@@ -1,32 +1,100 @@
 $(document).ready(function() {
 
+
+
+    var color1 = "#ffffcc";
+    var color2 = "#ffeda0";
+    var color3 = "#fed976";
+    var color4 = "#feb24c";
+    var color5 = "#fd8d3c";
+    var color6 = "#fc4e2a";
+    var color7 = "#e31a1c";
+    var color8 = "#bd0026";
+    var color9 = "#800026";
+
+
+
     function parseDate(str_date) {
         return new Date(Date.parse(str_date));
     }
 
     function minutesToIcon(minutes) {
-        if (minutes < 60) {
-            return iconRed;
+        if (minutes < 2) {
+            return icon9;
+        }
+        else if (minutes < 5) {
+            return icon8;
+        }
+        else if (minutes < 15) {
+            return icon7;
+        }
+        else if (minutes < 45) {
+            return icon6;
+        }
+        else if (minutes < 120) {
+            return icon5;
+        }
+        else if (minutes < 60*5) {
+            return icon4;
+        }
+        else if (minutes < 60*12) {
+            return icon3;
+        }
+        else if (minutes < 60*24) {
+            return icon2;
         }
         else {
-            return iconBlue;
+            return icon1;
         }
     }
 
-    var iconRed = L.MakiMarkers.icon({
-        color: "#EA1010",
+    var icon9 = L.MakiMarkers.icon({
+        color: color9,
         size: "m"
     });
 
-    var iconBlue = L.MakiMarkers.icon({
-        color: "#86989F",
+    var icon8 = L.MakiMarkers.icon({
+        color: color8,
         size: "m"
     });
 
+    var icon7 = L.MakiMarkers.icon({
+        color: color7,
+        size: "m"
+    });
+
+    var icon6 = L.MakiMarkers.icon({
+        color: color6,
+        size: "m"
+    });
+
+    var icon5 = L.MakiMarkers.icon({
+        color: color5,
+        size: "m"
+    });
+
+    var icon4 = L.MakiMarkers.icon({
+        color: color4,
+        size: "m"
+    });
+
+    var icon3 = L.MakiMarkers.icon({
+        color: color3,
+        size: "m"
+    });
+
+    var icon2 = L.MakiMarkers.icon({
+        color: color2,
+        size: "m"
+    });
+
+    var icon1 = L.MakiMarkers.icon({
+        color: color1,
+        size: "m"
+    });
 
     function populateMap() {
         userZonesWithTimestamps = jQuery.parseJSON(userZonesWithTimestamps);
-        console.log(userZonesWithTimestamps);
         var data = {
             "zones": userZones,
         };
