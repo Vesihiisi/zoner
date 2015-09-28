d67770dad66ae77f69cf750cdf29b5e16ad8d1a2
Vesihiisi <alicia@fagerving.se>
2015-09-28 19:34:36 +0200

    clean up unnecessary stuff

--- header.php
+++ header.php
@@ -1,25 +1,8 @@
-<?php 
-$currentPage = basename($_SERVER['PHP_SELF']);
-if ($currentPage == "zoneinfo.php") {
-    $formName = "z";
-} elseif ($currentPage == "userinfo.php") {
-    $formName = "u";
-}
-
-function highlightNavbar($uri)
-{
-    if ($uri == basename(strtok($_SERVER["REQUEST_URI"], '?'))) {
-        return "class=here";
-    }
-}
-
-?>
-
 <header>
 <div class="header-content">
-<span class="logo"><h1>.</h1></span>
+<span class="logo"><h1>ZoneInfo</h1></span>
     <form name="searchForm" id="searchForm">
-    <input type="text" name="<?php echo $formName;?>" id="<?php echo $formName;?>" placeholder="Enter zone name" value="" autofocus>
+    <input type="text" name="z" id="z" placeholder="Enter zone name" value="" autofocus>
     <input type="button" value="Search" id="search">
 </form>
 </div>
